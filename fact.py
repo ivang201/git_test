b0c3d12c9a1ffd4776539127226ad2ebd15539ec
Ivan Potanin <ivanupi887@gmail.com>
2024-03-12 19:12:28 +0300

    new fix

--- fact.py
+++ fact.py
@@ -1,4 +1,4 @@
 from factorial_recursive import factorial_recursive, minys;
-x = int(input())
-y = int(input())
+x,y = int(input())
 print(minys(factorial_recursive(y)), minys(factorial_recursive(x)))
+#пофиксил
