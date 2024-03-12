cd5214c42f7520d2263a6fa93f85211c772c18ae
Ivan Potanin <ivanupi887@gmail.com>
2024-03-12 21:14:28 +0300

    fix print

--- fact.py
+++ fact.py
@@ -1,5 +1,5 @@
 from factorial_recursive import factorial_recursive, minys, plus;
 x = int(input())
 y = int(input())
-print(minys(factorial_recursive(y)), minys(factorial_recursive(x)))
-print(plus(factorial_recursive(y)), minys(factorial_recursive(x)))
+print(minys(factorial_recursive(y), factorial_recursive(x)))
+print(plus(factorial_recursive(y), factorial_recursive(x)))
