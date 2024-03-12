04026a5abac29ba2265d8cee6a105776e83c367c
Ivan Potanin <ivanupi887@gmail.com>
2024-03-12 21:08:30 +0300

    new master

--- fact.py
+++ fact.py
@@ -2,4 +2,3 @@ from factorial_recursive import factorial_recursive, minys, plus;
 x,y = int(input())
 print(minys(factorial_recursive(y)), minys(factorial_recursive(x)))
 print(plus(factorial_recursive(y)), minys(factorial_recursive(x)))
-#пофиксил
